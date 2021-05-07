4fa79f217a55ad72cad522078c9c732becedd74a
Paul <p.kormanovskiy@gmail.com>
2021-05-07 23:13:15 +0300

    doesn't work

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('Here is a live server');
+window.addEventListener('load', ()=>{
+    let long;
+    let lat;
+    const APIkey = '1a3b4d0b03c39a9a9874022ceeec08f3';
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            
+            let api = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${APIkey}`;
+
+            fetch(api)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                })
+
+        })
+    } else {
+
+    }
+})
